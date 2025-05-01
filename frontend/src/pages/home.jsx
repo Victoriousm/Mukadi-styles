9a93d9a217c018291f27e0369ffbc24e6c363d40
Victoriousm <mukadiamvictorious@gmail.com>
2025-05-01 22:21:55 +0300

    After running build

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller'
+import BestSeller from '../components/Bestseller'
 import OurPolicy from '../components/OurPolicy'
 import NewsLetterBox from '../components/NewsLetterBox'
 
